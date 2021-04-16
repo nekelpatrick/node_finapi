31f17962036ffce50081c5d789858563f6e021a9
Patrick <nekelpatrick@gmail.com>
2021-04-16 09:54:53 -0300

    :sparkles: Added feat: added verifyaccount midddleware, code is cleaner now.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
-const express = require("express");
+const express = require('express')
 
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const customers = [];
+const customers = []
 
-app.post("/account", (request, response) => {
-  const { cpf, name } = request.body;
+function verifyIfExistsAccountByCPF (request, response, next) {
+  const { cpf } = request.headers
 
-  const customerAlreadyExists = customers.some(
-    (customer) => customer.cpf === cpf
-  );
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({
+      error: 'Customer not found'
+    })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
 
   if (customerAlreadyExists) {
-    return response.status(400).json({ error: "Customer already exists!" });
+    return response.status(400).json({ error: 'Customer already exists!' })
   }
 
   customers.push({
     cpf,
     name,
     id: uuidv4(),
-    statement: [],
-  });
+    statement: []
+  })
 
-  return response.status(201).send();
-});
+  return response.status(201).send()
+})
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+app.get('/statement', verifyIfExistsAccountByCPF, (request, response) => {
+  const { customer } = request
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
+  return response.json(customer.statement)
+})
 
-  if (!customer) {
-    response.status(400).json({
-      error: "Customer not found",
-    });
-  }
-
-  return response.json(customer.statement);
-});
-
-app.listen(3333);
+app.listen(3333)
